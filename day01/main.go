7a1714397b5d75cc8a9e290f8588b26fd9efba9a
felix <felix@newbit.global>
2019-09-19 20:08:34 +0800

    day01

---
+++ day01/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	// "./router"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	g := gin.New()
+
+	middlewares := []gin.handlerFunc{}
+
+	// router.Load(
+	// 	g,
+	// 	middlewares...,
+	// )
+
+	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
+	log.Printf(http.ListenAndServe(":8080", g).Error())
+}
